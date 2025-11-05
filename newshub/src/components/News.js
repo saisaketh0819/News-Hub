a5cd4ed96fdb8ea59bdbf1ab452959052d7f6806
saisaketh0819 <saisaketh0819@gmail.com>
2025-11-05 18:12:44 +0530

    Add React Router for navigation and enhance News component
    - Integrated react-router-dom for routing between different news categories.
    - Updated NavBar to use Link components for navigation.
    - Modified News component to include author and date information.
    - Improved NewsItem display with source badge and author/date details.

--- newshub/src/components/News.js
+++ newshub/src/components/News.js
@@ -2,68 +2,60 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
-    
 
-console.log("API Key: ", process.env.REACT_APP_API_KEY);
+//console.log("API Key: ", process.env.REACT_APP_API_KEY);
 
 export default class extends Component {
     static defaultProps = {
-        pageSize: 19,
+        pageSize: 18,
         country: 'in',
-        category: 'general' 
+        category: 'general',
+        author: 'unknown',
+        date: 'unknown'
     }
     static propTypes = {
         pageSize: PropTypes.number,
         country: PropTypes.string,
-        category: PropTypes.string
+        category: PropTypes.string,
+        author: PropTypes.string,
+        date: PropTypes.string
     }
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} - NewsHub`; 
     }
-
-    async componentDidMount() {
+    async updateNews(){
         this.setState({loading: true});
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=1&pageSize=${this.props.pageSize}`);    
+        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${this.state.page}&pageSize=${this.props.pageSize}`);   
         let parsedData = await data.json()
         this.setState({loading: false});
         this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
     }
+    async componentDidMount() {
+        this.updateNews();
+    }
     handlePrevClick = async ()=>{
-        this.setState({loading: true});
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${this.state.page-1}&pageSize=${this.props.pageSize}`);    
-        let parsedData = await data.json()
-        this.setState({loading: false});
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles
-        })
+        this.setState({page: this.state.page - 1});
+        this.updateNews();
     }   
     handleNextClick = async ()=>{
-        if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/19))){
-            this.setState({loading: true});
-            let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`);    
-            let parsedData = await data.json()
-            this.setState({loading: false});
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles}
-            )
-        }
+        this.setState({page: this.state.page + 1});
+        this.updateNews();
     }
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin:'35px 0px'}}>NewsHub - Top Headlines</h1>
+        <h1 className="text-center" style={{margin:'35px 0px'}}>NewsHub Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h1>
         {this.state.loading&&<Spinner/>}
         <div className="row">
         {!this.state.loading&&this.state.articles.map((element)=>{
             return <div className="col-md-4"  key={element.url}>
-                    <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                 </div>
         })}
         </div>
--- newshub/src/components/NewsItem.js
+++ newshub/src/components/NewsItem.js
@@ -5,11 +5,13 @@ export default class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
             <img src={!imageUrl?"https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
+                <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
